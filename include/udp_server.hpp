2679df58f5da827dd6e5df07e7baa5e674516a08
k.sato <bjtfm000@gmail.com>
2025-08-14 01:30:24 +0900

    fix 20250813_14

--- include/udp_server.hpp
+++ include/udp_server.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <config.hpp>
 #define FMT_HEADER_ONLY
 #include <fmt/core.h>
 #define ASIO_STANDALONE
@@ -44,7 +45,7 @@ public:
 
 private:
     void configure_socket(udp::socket& sock) {
-        int rcvbuf_size = 64 * 1024;  // 64KB
+        int rcvbuf_size = config::UDP_SO_RCVBUF
         int fd = sock.native_handle();
         if (setsockopt(fd, SOL_SOCKET, SO_RCVBUF, &rcvbuf_size, sizeof(rcvbuf_size)) < 0) {
             perror("setsockopt(SO_RCVBUF) failed");
